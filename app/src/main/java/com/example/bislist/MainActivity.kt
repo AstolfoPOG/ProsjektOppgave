1c4dad0166c9964c63935aef3078e5f7a82dd162
Kristian Utvær <kristianu@uia.no>
2021-03-26 15:34:48 +0100

    Lagt til funksjoner for taska og cardview

--- app/src/main/java/com/example/bislist/MainActivity.kt
+++ app/src/main/java/com/example/bislist/MainActivity.kt
@@ -1,23 +1,30 @@
 package com.example.bislist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bislist.databinding.ActivityMainBinding
-import com.example.bislist.tasks.TaskCollectionAdapter
+import com.example.bislist.tasks.TaskRecyclerAdapter
+import com.example.bislist.tasks.TaskDepoManger
+import com.example.bislist.tasks.TaskDetailActivity
 import com.example.bislist.tasks.data.Task
+
+
+const val EXTRA_TASK_INFO: String = "com.example.bislist.task.info"
+
+class TaskHolder{
+
+    companion object{
+        var PickedTask:Task? = null
+    }
+
+}
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-    private lateinit var taskAdapter:TaskCollectionAdapter
-
-    private val taskCollection:MutableList<Task> = mutableListOf(
-            Task("Handle Liste"),
-            Task("Film Liste"),
-            Task("Huske Liste")
-    )
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +33,46 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.taskListing.layoutManager = LinearLayoutManager(this)
-        binding.taskListing.adapter = TaskCollectionAdapter(taskCollection)
+        binding.taskListing.adapter = TaskRecyclerAdapter(emptyList<Task>(), this::onTaskClicked)
+
+        TaskDepoManger.instance.onTask = {
+            (binding.taskListing.adapter as TaskRecyclerAdapter).updateCollection(it)
+        }
+
+        TaskDepoManger.instance.loadTask()
+
+        binding.saveBt.setOnClickListener {
+            val title = binding.title.text.toString()
+            val state = false
+
+            binding.title.setText("")
+
+            addTask(title,state)
+        }
+
     }
+
+
+    private fun addTask(title: String, state: Boolean){
+        val task = Task(title, state)
+        if (title.isNotEmpty())
+            TaskDepoManger.instance.addTask(task)
+        else{
+            Toast.makeText(this, "enter text", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
+    private fun onTaskClicked(task: Task): Unit{
+
+
+        TaskHolder.PickedTask = task
+
+        val intent = Intent(this, TaskDetailActivity::class.java)
+
+        startActivity(intent)
+
+    }
+
 }
--- app/src/main/java/com/example/bislist/tasks/data/Task.kt
+++ app/src/main/java/com/example/bislist/tasks/data/Task.kt
@@ -1,3 +1,9 @@
 package com.example.bislist.tasks.data
 
-data class Task(val titel:String)
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+data class Task(val titel:String, val state:Boolean): Parcelable
+
